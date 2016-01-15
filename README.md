4ecade8a42941b878528fdc574d45dcaee491dd0
Dan <dan@edmondscommerce.co.uk>
2016-01-15 12:26:09 +0000

    Renamed context to naming convention, updated README and removed a dependency

---
+++ README.md
@@ -0,0 +1,25 @@
+#Behat Error Handling Context
+## By [Edmonds Commerce](https://www.edmondscommerce.co.uk)
+
+Used to handle simple errors and to take screenshots when they occur
+
+### Installation
+
+Install via composer
+
+"edmondscommerce/behat-error-handling-context": "~1.0"
+
+
+### Include Context in Behat Configuration
+
+```
+default:
+    # ...
+    suites:
+        default:
+            # ...
+            contexts:
+                - # ...
+                - EdmondsCommerce\BehatErrorHandling\ErrorHandlingContext
+
+```
--- src/ErrorHandlingContext.php
+++ src/ErrorHandlingContext.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace EdmondsCommerce\BehatErrorHandling;
+<?php namespace EdmondsCommerce\BehatErrorHandling;
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
@@ -8,7 +6,7 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 /**
  * Defines application features from the specific context.
  */
-class ErrorHandling extends Behat\MinkExtension\Context\RawMinkContext implements Context, SnippetAcceptingContext
+class ErrorHandlingContext extends Behat\MinkExtension\Context\RawMinkContext implements Context, SnippetAcceptingContext
 {
     /**
      * Allow Behat to be killed after the first failed scenario
@@ -18,8 +16,10 @@ class ErrorHandling extends Behat\MinkExtension\Context\RawMinkContext implement
      */
     public function dieOnFailedScenario(Behat\Behat\Hook\Scope\AfterScenarioScope $scope)
     {
-        if (99 === $scope->getTestResult()->getResultCode()) {
-            if (isset($_SERVER['BEHAT_DIE_ON_FAILURE'])) {
+        if (99 === $scope->getTestResult()->getResultCode())
+        {
+            if (isset($_SERVER['BEHAT_DIE_ON_FAILURE']))
+            {
                 die("BEHAT_DIE_ON_FAILURE is defined\nKilling Full Process");
             }
         }
@@ -36,19 +36,22 @@ class ErrorHandling extends Behat\MinkExtension\Context\RawMinkContext implement
      */
     public function myAfterStepHook(Behat\Behat\Hook\Scope\AfterStepScope $scope)
     {
-        if (99 === $scope->getTestResult()->getResultCode()) {
+        if (99 === $scope->getTestResult()->getResultCode())
+        {
             $driver = $this->getSession()->getDriver();
-            if ($driver instanceof \Behat\Mink\Driver\Selenium2Driver) {
+            if ($driver instanceof \Behat\Mink\Driver\Selenium2Driver)
+            {
 
                 $name = preg_replace(
                     '%[^a-z0-9]%i',
                     '_',
-                    array_pop($_SERVER['argv']).':'.$scope->getStep()->getText().'_'.$driver->getCurrentUrl()
+                    array_pop($_SERVER['argv']) . ':' . $scope->getStep()->getText() . '_' . $driver->getCurrentUrl()
                 );
-                if(!file_exists('/tmp/behat')) {
+                if (!file_exists('/tmp/behat'))
+                {
                     mkdir('/tmp/behat/');
                 }
-                $file = '/tmp/behat/'.$name.'.png';
+                $file = '/tmp/behat/' . $name . '.png';
                 file_put_contents($file, $this->getSession()->getDriver()->getScreenshot());
                 echo "Error Screen Shot Saved to $file";
                 xdebug_break();
